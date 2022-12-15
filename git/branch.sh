231ad54bba2f0d02b618a978f5c5d7ff5485c2e5
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-12-15 22:11:49 +0200

    updated src code

---
+++ git/branch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2022 Mahmoud Mohamed (00xWolf)  <https://github.com/mmsaeed509>
+# LICENSE © GNU-GPL3
+#
+
+git branch -m main master
+git fetch origin
+git branch -u origin/master master
+git remote set-head origin -a
+
+"       碌                                             "
+"     󰮯    ¥ "
---
+++ pkg/updatePackages.ps1
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2022 Mahmoud Mohamed (00xWolf) <https://github.com/mmsaeed509>
+# LICENSE © GNU-GPL3
+#
+
+<# Variables & Array #>
+$WORKING_DIR = pwd # to work in right directory #
+
+<# Array to store the needed directories to update their packages #>
+$DIRECTORIES = @("apikey-manager","auth","checkout","customer","emailing","kms","manage-business","payment","tokenization")
+
+Write-Host ""
+Write-Host "##########################" -ForegroundColor DarkBlue
+Write-Host "# Update Packages Script #" -ForegroundColor DarkBlue
+Write-Host "##########################" -ForegroundColor DarkBlue
+Write-Host ""
+
+<# here we loop in all directories one by one and update their packages #>
+foreach ($PACKAGE in $DIRECTORIES) {
+    
+    Write-Host "updating packages in $PACKAGE ........ " -ForegroundColor Red
+    cd $WORKING_DIR/$PACKAGE
+    npm run update:common
+    Write-Host "$PACKAGE packages updated successfully!" -ForegroundColor Green
+    Write-Host ""
+
+}
+
+cd $WORKING_DIR # Return to working directory #
+
+Write-Host ""
+Write-Host "#####################" -ForegroundColor Green
+Write-Host "#      D O N E!     #" -ForegroundColor Green
+Write-Host "#####################" -ForegroundColor Green
