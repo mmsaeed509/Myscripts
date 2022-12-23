eb8ad71fbcd408f68d6ee9615a655ca05dfac5db
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-12-23 21:27:16 +0200

    updated src code

--- colors/COLORS.java
+++ colors/COLORS.java
@@ -34,23 +34,23 @@ public class COLORS {
     public static final String CYAN_BACKGROUND   = "\u001B[46m";
 
     /* Bold Colors */
-    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
-    public static final String RED_BOLD = "\033[1;31m";    // RED
-    public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
-    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
-    public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
-    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
-    public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
+    public static final String BLACK_BOLD        = "\033[1;30m";
+    public static final String RED_BOLD          = "\033[1;31m";
+    public static final String GREEN_BOLD        = "\033[1;32m";
+    public static final String YELLOW_BOLD       = "\033[1;33m";
+    public static final String BLUE_BOLD         = "\033[1;34m";
+    public static final String PURPLE_BOLD       = "\033[1;35m";
+    public static final String CYAN_BOLD         = "\033[1;36m";
+    public static final String WHITE_BOLD        = "\033[1;37m";
 
     /* Underline Colors */
-    public static final String BLACK_UNDERLINED = "\033[4;30m";  // BLACK
-    public static final String RED_UNDERLINED = "\033[4;31m";    // RED
-    public static final String GREEN_UNDERLINED = "\033[4;32m";  // GREEN
-    public static final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
-    public static final String BLUE_UNDERLINED = "\033[4;34m";   // BLUE
-    public static final String PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
-    public static final String CYAN_UNDERLINED = "\033[4;36m";   // CYAN
-    public static final String WHITE_UNDERLINED = "\033[4;37m";  // WHITE
+    public static final String BLACK_UNDERLINED  = "\033[4;30m";
+    public static final String RED_UNDERLINED    = "\033[4;31m";
+    public static final String GREEN_UNDERLINED  = "\033[4;32m";
+    public static final String YELLOW_UNDERLINED = "\033[4;33m";
+    public static final String BLUE_UNDERLINED   = "\033[4;34m";
+    public static final String PURPLE_UNDERLINED = "\033[4;35m";
+    public static final String CYAN_UNDERLINED   = "\033[4;36m";
+    public static final String WHITE_UNDERLINED  = "\033[4;37m";
 
 }
