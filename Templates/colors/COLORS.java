3940d972f78f627d2f713967c68188c75e88e2a2
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2023-04-11 04:10:21 +0200

    updated src code

--- Templates/colors/COLORS.java
+++ Templates/colors/COLORS.java
@@ -6,11 +6,12 @@
  * 
  * */
 
-public class COLORS {
+ public class COLORS {
 
     /* COLORS */
     public static final String RESET_COLOR = "\u001B[0m";
 
+    /* Regular Colors */
     public static final String BLACK_FOREGROUND  = "\u001B[30m";
     public static final String WHITE_FOREGROUND  = "\u001B[37m";
     public static final String RED_FOREGROUND    = "\u001B[31m";
@@ -20,6 +21,57 @@ public class COLORS {
     public static final String PURPLE_FOREGROUND = "\u001B[35m";
     public static final String CYAN_FOREGROUND   = "\u001B[36m";
 
+    /* Bold Colors */
+    public static final String BLACK_BOLD  = "\u001B[30;1m";
+    public static final String WHITE_BOLD  = "\u001B[37;1m";
+    public static final String RED_BOLD    = "\u001B[31;1m";
+    public static final String GREEN_BOLD  = "\u001B[32;1m";
+    public static final String YELLOW_BOLD = "\u001B[33;1m";
+    public static final String BLUE_BOLD   = "\u001B[34;1m";
+    public static final String PURPLE_BOLD = "\u001B[35;1m";
+    public static final String CYAN_BOLD   = "\u001B[36;1m";
+
+    /* Underline Colors */
+    public static final String BLACK_UNDERLINE  = "\u001B[30;4m";
+    public static final String WHITE_UNDERLINE  = "\u001B[37;4m";
+    public static final String RED_UNDERLINE    = "\u001B[31;4m";
+    public static final String GREEN_UNDERLINE  = "\u001B[32;4m";
+    public static final String YELLOW_UNDERLINE = "\u001B[33;4m";
+    public static final String BLUE_UNDERLINE   = "\u001B[34;4m";
+    public static final String PURPLE_UNDERLINE = "\u001B[35;4m";
+    public static final String CYAN_UNDERLINE   = "\u001B[36;4m";
+
+    /* High Intensity Colors */
+    public static final String BLACK_HIGH_INTENSITY  = "\u001B[90m";
+    public static final String RED_HIGH_INTENSITY    = "\u001B[91m";
+    public static final String GREEN_HIGH_INTENSITY  = "\u001B[92m";
+    public static final String YELLOW_HIGH_INTENSITY = "\u001B[93m";
+    public static final String BLUE_HIGH_INTENSITY   = "\u001B[94m";
+    public static final String PURPLE_HIGH_INTENSITY = "\u001B[95m";
+    public static final String CYAN_HIGH_INTENSITY   = "\u001B[96m";
+    public static final String WHITE_HIGH_INTENSITY  = "\u001B[97m";
+
+    /* Bold High Intensity Colors */
+    public static final String BLACK_BOLD_HIGH_INTENSITY  = "\u001B[90;1m";
+    public static final String RED_BOLD_HIGH_INTENSITY    = "\u001B[91;1m";
+    public static final String GREEN_BOLD_HIGH_INTENSITY  = "\u001B[92;1m";
+    public static final String YELLOW_BOLD_HIGH_INTENSITY = "\u001B[93;1m";
+    public static final String BLUE_BOLD_HIGH_INTENSITY   = "\u001B[94;1m";
+    public static final String PURPLE_BOLD_HIGH_INTENSITY = "\u001B[95;1m";
+    public static final String CYAN_BOLD_HIGH_INTENSITY   = "\u001B[96;1m";
+    public static final String WHITE_BOLD_HIGH_INTENSITY  = "\u001B[97;1m";
+
+    /* High Intensity Background Colors */
+    public static final String BLACK_BACKGROUND_HIGH_INTENSITY  = "\u001B[100;5m";
+    public static final String RED_BACKGROUND_HIGH_INTENSITY    = "\u001B[101;5m";
+    public static final String GREEN_BACKGROUND_HIGH_INTENSITY  = "\u001B[102;5m";
+    public static final String YELLOW_BACKGROUND_HIGH_INTENSITY = "\u001B[103;5m";
+    public static final String BLUE_BACKGROUND_HIGH_INTENSITY   = "\u001B[104;5m";
+    public static final String PURPLE_BACKGROUND_HIGH_INTENSITY = "\u001B[105;5m";
+    public static final String CYAN_BACKGROUND_HIGH_INTENSITY   = "\u001B[106;5m";
+    public static final String WHITE_BACKGROUND_HIGH_INTENSITY  = "\u001B[107;5m";
+
+    /* Background Colors */
     public static final String BLACK_BACKGROUND  = "\u001B[40m";
     public static final String WHITE_BACKGROUND  = "\u001B[47m";
     public static final String RED_BACKGROUND    = "\u001B[41m";
