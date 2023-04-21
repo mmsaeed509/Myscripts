0162aa40c0b384b22b7bbeae45826651144f2e16
Mahmoud Mohamed (00xWolf - mmsaeed509) <mmsaeed509@gmail.com>
2023-04-21 09:41:56 +0200

    updated src code

--- git/PAT_CONFIG.sh
+++ git/PAT_CONFIG.sh
@@ -8,5 +8,14 @@
 USERNAME="Mahmoud Mohamed (00xWolf - mmsaeed509)"
 E_MAIL="mmsaeed509@gmail.com"
 
+# also, you can edit from < ~/.gitconfig > #
 git config --global user.name ${USERNAME}
 git config --global user.email ${E_MAIL}
+
+# overall system, also you can edit from < /etc/gitconfig > #
+git config --system user.name ${USERNAME}
+git config --system user.email ${E_MAIL}
+
+# Project ONLY, also you can edit from < /.git/config > #
+git config user.name ${USERNAME}
+git config user.email ${E_MAIL}
