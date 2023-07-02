3782676c15de19bf14aa4d1f9c57fe008f98b187
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2023-07-02 19:30:33 +0300

    updated src code
    Update PAT_CONFIG.sh

--- git/PAT_CONFIG.sh
+++ git/PAT_CONFIG.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-#
-# Copyright (C) 2022 Mahmoud Mohamed (00xWolf)  <https://github.com/mmsaeed509>
-# LICENSE © GNU-GPL3
-#
+#####################################
+#                                   #
+#  @author      : 00xWolf           #
+#    GitHub    : @mmsaeed509       #
+#    Developer : Mahmoud Mohamed   #
+#  﫥  Copyright : Exodia OS        #
+#                                   #
+#####################################
 
-USERNAME="Mahmoud Mohamed (00xWolf - mmsaeed509)"
+USERNAME="Mahmoud Mohamed (00xWolf - Gmail/GitHub: @mmsaeed509)"
 E_MAIL="mmsaeed509@gmail.com"
 
 # also, you can edit from < ~/.gitconfig > #
@@ -13,8 +17,8 @@ git config --global user.name ${USERNAME}
 git config --global user.email ${E_MAIL}
 
 # overall system, also you can edit from < /etc/gitconfig > #
-git config --system user.name ${USERNAME}
-git config --system user.email ${E_MAIL}
+sudo git config --system user.name ${USERNAME}
+sudo git config --system user.email ${E_MAIL}
 
 # Project ONLY, also you can edit from < /.git/config > #
 git config user.name ${USERNAME}
@@ -22,3 +26,6 @@ git config user.email ${E_MAIL}
 
 # set the default editor for commit msg #
 git config --global core.editor "nvim"
+
+# raising the postBuffer size #
+git config --global http.postBuffer 524288000
