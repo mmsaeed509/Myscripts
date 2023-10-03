85e386bb3d0d1c981df778b61e4e7b69d5b5832c
Mahmoud Mohamed (00xWolf - Gmail/GitHub: @mmsaeed509) <mmsaeed509@gmail.com>
2023-10-03 20:59:48 +0300

    updated/added  M pkg/updatePackages.ps1 ?? pkg/updatePackages-old.ps1

--- pkg/updatePackages.ps1
+++ pkg/updatePackages.ps1
@@ -1,7 +1,11 @@
-#
-# Copyright (C) 2022 Mahmoud Mohamed (00xWolf) <https://github.com/mmsaeed509>
-# LICENSE © GNU-GPL3
-#
+#####################################
+#                                   #
+#  @author      : 00xWolf           #
+#    GitHub    : @mmsaeed509       #
+#    Developer : Mahmoud Mohamed   #
+#  﫥  Copyright : Exodia OS         #
+#                                   #
+#####################################
 
 <# Variables & Array #>
 $WORKING_DIR = pwd # to work in right directory #
@@ -10,18 +14,16 @@ $WORKING_DIR = pwd # to work in right directory #
 $DIRECTORIES = @("apikey-manager","auth","checkout","customer","emailing","kms","manage-business","payment","tokenization")
 
 Write-Host ""
-Write-Host "##########################" -ForegroundColor DarkBlue
-Write-Host "# Update Packages Script #" -ForegroundColor DarkBlue
-Write-Host "##########################" -ForegroundColor DarkBlue
+Write-Host "[*] Update Packages Script" -ForegroundColor DarkBlue
 Write-Host ""
 
 <# here we loop in all directories one by one and update their packages #>
 foreach ($PACKAGE in $DIRECTORIES) {
     
-    Write-Host "updating packages in $PACKAGE ........ " -ForegroundColor Red
+    Write-Host "   ==> updating $PACKAGE packages... " -ForegroundColor Red
     cd $WORKING_DIR/$PACKAGE
     npm run update:common
-    Write-Host "$PACKAGE packages updated successfully!" -ForegroundColor Green
+    Write-Host "   [✔] $PACKAGE packages updated successfully!" -ForegroundColor Green
     Write-Host ""
 
 }
@@ -29,6 +31,5 @@ foreach ($PACKAGE in $DIRECTORIES) {
 cd $WORKING_DIR # Return to working directory #
 
 Write-Host ""
-Write-Host "#####################" -ForegroundColor Green
-Write-Host "#      D O N E!     #" -ForegroundColor Green
-Write-Host "#####################" -ForegroundColor Green
+Write-Host "[✔]    D O N E!" -ForegroundColor Green
+Write-Host ""
