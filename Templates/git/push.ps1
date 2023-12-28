931723c529dd1aa9549552783ed09b60c6a67e28
Mahmoud Mohamed (00xWolf - Gmail/GitHub: @mmsaeed509) <mmsaeed509@gmail.com>
2023-12-28 19:56:01 +0200

    \033[1;36m==> modified : Templates/git/push.ps1 \n\033[0m    \033[1;36m==> modified : Templates/git/push.sh \n\033[0m    \033[1;36m==> modified : push.ps1 \n\033[0m    \033[1;32m==> added : Templates/Pipelines/.gitlab-ci.yml \n\033[0m    \033[1;32m==> added : Templates/Pipelines/config.toml \n\033[0m    \033[1;32m==> added : Templates/git/.gitignore \n\033[0m

--- Templates/git/push.ps1
+++ Templates/git/push.ps1
@@ -1,7 +1,11 @@
-#
-# Copyright (C) 2022 Mahmoud Mohamed (00xWolf)  <https://github.com/mmsaeed509>
-# LICENSE © GNU-GPL3
-#
+#####################################
+#                                   #
+#  @author      : 00xWolf           #
+#    GitHub    : @mmsaeed509       #
+#    Developer : Mahmoud Mohamed   #
+#  﫥  Copyright : Exodia OS         #
+#                                   #
+#####################################
 
 #
 # you can run script with 2 arguments (your commit comment)
@@ -10,50 +14,79 @@
 
 # a simple PowerShell script to push your commits to GitHub #
 
-Write-Host "#############################" -ForegroundColor Cyan
-Write-Host "#      Git Push Script      #" -ForegroundColor Cyan
-Write-Host "#############################" -ForegroundColor Cyan
 
-
-# get branch name (e.g master, main, etc... ) #
-$Branch = git branch --show-current
-
-Write-Host "`n[*] Your Current Branch : $Branch" -ForegroundColor Red
-
-# get new updates if it founded #
-Write-Host "`n[+] Updating Repo... `n" -ForegroundColor Magenta
-git pull 
-
-Write-Host "`n[+] The new changes in the repo:- `n" -ForegroundColor Magenta
-git status -s
-
-Write-Host "`n[+] Adding new changes to the repo... `n" -ForegroundColor Magenta
-git add --all .
-
-$M = "-m"
-
-if ( "$($args[0])" -eq $M ) {
-
-    # commit changes#
-    Write-Host ""
-    git commit -m "$($args[1])"
-
-}else {
-    
-    Write-Host "##################################" -ForegroundColor Magenta
-    Write-Host "# Write your commit comment! :-  #" -ForegroundColor Magenta
-
-    $COMMIT = Read-Host
-
-    # commit changes#
-    Write-Host ""
-    git commit -m "$COMMIT"
-
+# Function to print colored text #
+function Write-ColoredText {
+    param (
+        [string]$text,
+        [string]$color
+    )
+    Write-Host $text -ForegroundColor $color
 }
 
-# push to repo #
+Write-ColoredText "#############################" "Cyan"
+Write-ColoredText "#      Git Push Script      #" "Cyan"
+Write-ColoredText "#############################" "Cyan"
+
+# Get branch name #
+$Branch = git rev-parse --abbrev-ref HEAD
+
+# Default commit message based on changes #
+$DEFAULT_COMMIT_MSG = ""
+
+# Get Git status #
+git status -s | ForEach-Object {
+    $status = $_.Substring(0, 2)
+    $file = $_.Substring(3).Trim()
+
+    switch ($status) {
+        "D" {
+            # Deleted #
+            $DEFAULT_COMMIT_MSG += "    ==> deleted : $file `n"
+            break
+        }
+        "M" {
+            # Modified #
+            $DEFAULT_COMMIT_MSG += "    ==> modified : $file `n"
+            break
+        }
+        "??" {
+            # Added #
+            $DEFAULT_COMMIT_MSG += "    ==> added : $file `n"
+            break
+        }
+    }
+}
+
+# If no changes detected, use a default message #
+if (-not $DEFAULT_COMMIT_MSG) {
+    $DEFAULT_COMMIT_MSG = "   ==> NO changes"
+}
+
+Write-ColoredText "`n[*] Your Current Branch : $Branch" "Red"
+
+# Get new updates if they exist #
+Write-ColoredText "`n[+] Updating Repo... `n" "Magenta"
+git pull
+
+Write-ColoredText "`n[+] The new changes in the repo:`n`n$DEFAULT_COMMIT_MSG" "Magenta"
+
+Write-ColoredText "`n[+] Adding new changes to the repo... `n" "Magenta"
+git add --all .
+
+if ($args[0] -eq "-m") {
+    # Commit changes #
+    Write-Host ""
+    git commit -m $args[1]
+} else {
+    # Use the default commit message #
+    Write-Host ""
+    git commit -m $DEFAULT_COMMIT_MSG
+}
+
+# Push to repo #
 Write-Host ""
 git push -u origin $Branch
 
-# D O N E! #
-Write-Host "`n[✔] D O N E `n" -ForegroundColor Green
+# Done #
+Write-ColoredText "`n[✔] D O N E `n" "Green"
